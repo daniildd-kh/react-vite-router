e9f60a57593134dae97e110279a0877301859cb5
Khromov Daniil <khromov865@mail.ru>
2025-01-21 14:52:35 +0300

    feat: added register page

---
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,15 @@
+import Card from '@/containers/Card/Card';
+import Section from '@/containers/Section/Section';
+import RegisterForm from '@/screens/RegisterForm/RegisterForm';
+
+const RegisterPage = () => {
+  return (
+    <Section>
+    <Card>
+      <RegisterForm />
+    </Card>
+  </Section>
+  );
+};
+
+export default RegisterPage;
