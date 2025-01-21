dc9920edeb81b36552c1fa105b2274b3aef4986a
Khromov Daniil <khromov865@mail.ru>
2025-01-21 14:56:58 +0300

    feat: added routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,3 +1,9 @@
+import LoginPage from '@/pages/LoginPage/LoginPage';
+import MainPage from '@/pages/MainPage/MainPage';
+import RegisterPage from '@/pages/RegisterPage/RegisterPage';
+
 export const routes = [
-	{ index: true, element: <p>Hello world!</p> }
+	{ index: true, element: <MainPage /> },
+	{ path: '/login', element: <LoginPage /> },
+	{ path: '/register', element: <RegisterPage /> },
 ];
