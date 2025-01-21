6c2be7add0e3acb524d317bc1aca7403e6f1b442
Khromov Daniil <khromov865@mail.ru>
2025-01-21 09:35:06 +0300

    feat: added field component

---
+++ src/components/Field/Field.tsx
@@ -0,0 +1,53 @@
+import clsx from 'clsx';
+import { ChangeEvent, useEffect, useState } from 'react';
+import styles from './Field.module.scss';
+
+type FieldProps = {
+	className?: string;
+	text?: string;
+	value?: string;
+	type?: string;
+	onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+	placeholder?: string;
+	inputComponent?: React.ElementType;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+const Field = ({
+	className,
+	text,
+	value,
+	type,
+	onChange,
+	placeholder,
+	inputComponent,
+  ...props
+}: FieldProps) => {
+	const [valueInput, setValueInput] = useState(value);
+
+	useEffect(() => {
+		setValueInput(value || '');
+	}, [value]);
+
+	const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setValueInput(e.target.value);
+		onChange?.(e);
+	};
+
+	const Input = inputComponent || 'input';
+
+	return (
+		<label className={clsx(styles.label, className)}>
+			<span className={styles.span}>{text}</span>
+			<Input
+				className={styles.input}
+				type={type}
+				onChange={onInputChange}
+				value={valueInput}
+				placeholder={placeholder}
+        {...props}
+			/>
+		</label>
+	);
+};
+
+export default Field;
