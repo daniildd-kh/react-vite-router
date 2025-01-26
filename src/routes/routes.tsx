fde1c7c5f7347f325dc1c78de80096769bc5b781
Khromov Daniil <khromov865@mail.ru>
2025-01-26 22:21:50 +0300

    feat: added loader on the main page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,9 @@
 import LoginPage from '@/pages/LoginPage/LoginPage';
-import MainPage from '@/pages/MainPage/MainPage';
+import main from '@/pages/MainPage/MainPage';
 import RegisterPage from '@/pages/RegisterPage/RegisterPage';
 
 export const routes = [
-	{ index: true, element: <MainPage /> },
+	{ index: true, ...main },
 	{ path: '/login', element: <LoginPage /> },
 	{ path: '/register', element: <RegisterPage /> },
 ];
